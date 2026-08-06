26411bf4d1f319e064b8639761c137998ea9bb7a
Shivangi <shivangir23@iiserb.ac.in>
2026-08-06 11:34:57 +0530

    Initial commit

--- src/components/NewsCopy/NewsCopy.jsx
+++ src/components/NewsCopy/NewsCopy.jsx
@@ -169,7 +169,8 @@ const NewsCopySection = () => {
                 href="https://twitter.com/TwitterDev?ref_src=twsrc%5Etfw">
                 Tweets by TwitterDev
                 </a> */}
-                <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7302985963624894464" height="399" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+                <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7462374374390812672" height="399" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+                <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:7458579695907848192" height="399" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>                
 {/*                 
                 <a class="twitter-timeline" 
                 // data-width="100%" 
